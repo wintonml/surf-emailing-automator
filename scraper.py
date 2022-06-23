7483d951bff6a7bad4db6be9d286442424a0e46b
Michael Lennon Winton <wintonml@gmail.com>
2022-06-24 10:23:00 +1200

    1: Functionality to open a browser
    The script can now open a Firefox web browser and grab the title of it.
    This will be edited to be a different website. But this was just following a tutorial

--- scraper.py
+++ scraper.py
@@ -1,12 +1,17 @@
 # https://www.edureka.co/blog/web-scraping-with-python/#whywebscraping
+# https://medium.com/dropout-analytics/selenium-and-geckodriver-on-mac
+# -b411dbfe61bc
 # The link above was used to learn how to create a web scraper using Python.
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 import pandas as pd
 
-driver = webdriver.safari()
+driver_path = '/Users/michaellennonwinton/Downloads/geckodriver'
 
 sea_conditions = []
-driver.get("https://www.metservice.com/marine/regions/kapiti-wellington/surf"
-           "/locations/lyall-bay")
+driver = webdriver.Firefox(executable_path=driver_path)
+driver.get("https://www.python.org")
+
+print(driver.title)
