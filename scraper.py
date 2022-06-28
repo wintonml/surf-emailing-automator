c2cf078b18ffa9eed392a0eaa3755a46c9346025
Michael Lennon Winton <wintonml@gmail.com>
2022-06-28 22:55:42 +1200

    1: Able to go to surf forecast page.
    By using selenium the program is now able to go to the surf forecast page. However, it is not always reliable.
    It will need to be edited to not break. This could by done by using a polling function

--- scraper.py
+++ scraper.py
@@ -22,7 +22,15 @@ def wait_for_page_to_load(search):
         print("Timed out waiting for page to load")
 
 
-sea_conditions = []
+def wait_for_page_element(element):
+    print(f"Waiting for {element} to be present")
+    try:
+        element_present = Ec.element_to_be_clickable((By.LINK_TEXT, element))
+        wait.until(element_present)
+    except TimeoutException:
+        print("Timed out waiting for page to load")
+
+
 driver = webdriver.Firefox(executable_path=driver_path)
 driver.get("https://www.metservice.com/marine")
 wait = WebDriverWait(driver, timeout=30, poll_frequency=2)
@@ -32,3 +40,12 @@ area.click()
 wait_for_page_to_load("Kapiti and Wellington")
 surf = (driver.find_element(by=By.LINK_TEXT, value="Surf"))
 surf.click()
+wait_for_page_to_load("Surf")
+
+bay = driver.find_element(by=By.XPATH, value="//div[@title='Lyall Bay']")
+bay.click()
+
+wait_for_page_element("View full surf forecasts")
+location_forecast = (driver.find_element(by=By.LINK_TEXT,
+                                         value="View full surf forecasts"))
+location_forecast.click()
