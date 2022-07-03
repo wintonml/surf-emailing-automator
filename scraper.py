0d83fdcf4d75e269f90b9a2ed70115330e56592f
Michael Lennon Winton <wintonml@gmail.com>
2022-07-03 19:04:54 +1200

    fix_#1: pylint errors
    In the pipeline these errors were identified due to running pylint on the file.
    This included not using the correct naming conventions and that docstrings were missing from functions.

--- scraper.py
+++ scraper.py
@@ -1,7 +1,11 @@
-# https://www.edureka.co/blog/web-scraping-with-python/#whywebscraping
-# https://medium.com/dropout-analytics/selenium-and-geckodriver-on-mac
-# -b411dbfe61bc
-# The links above was used to learn how to create a web scraper using Python.
+"""
+This program will scrape the data from a surf forecast website.
+
+https://www.edureka.co/blog/web-scraping-with-python/#whywebscraping
+https://medium.com/dropout-analytics/selenium-and-geckodriver-on-mac
+-b411dbfe61bc
+The links above was used to learn how to create a web scraper using Python.
+"""
 from selenium import webdriver
 # from bs4 import BeautifulSoup
 # import pandas as pd
@@ -10,10 +14,19 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as Ec
 from selenium.common.exceptions import TimeoutException
 
-driver_path = '/Users/michaellennonwinton/Downloads/geckodriver'
+DRIVER_PATH = '/Users/michaellennonwinton/Downloads/geckodriver'
 
 
 def wait_for_page_to_load(search):
+    """
+    This waits for the webpage to load by waiting for the webpage's title to
+    present.
+
+    Parameter:
+        search (str): string that is being searched for
+
+    return: None
+    """
     print(f"Searching for {search}")
     try:
         element_present = Ec.title_contains(search)
@@ -23,6 +36,15 @@ def wait_for_page_to_load(search):
 
 
 def wait_for_page_element(element):
+    """
+    This waits for the webpage to load by waiting for an element on the
+    webpage to be clickable.
+
+    Parameter:
+        element (str): string that is being searched for
+
+    return: None
+    """
     print(f"Waiting for {element} to be present")
     try:
         element_present = Ec.element_to_be_clickable((By.LINK_TEXT, element))
@@ -31,7 +53,7 @@ def wait_for_page_element(element):
         print("Timed out waiting for page to load")
 
 
-driver = webdriver.Firefox(executable_path=driver_path)
+driver = webdriver.Firefox(executable_path=DRIVER_PATH)
 driver.get("https://www.metservice.com/marine")
 wait = WebDriverWait(driver, timeout=30, poll_frequency=2)
 webpage = driver.page_source
